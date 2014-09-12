a0da5ee5fff9d4a5090e4f7c7ebb3b1c3c687610
oskaremilsson <oskaremilsson@gmail.com>
2014-09-12 02:16:21 +0200

    Small cleanup, deleted unnecesary id since jQuery can take classes too.

--- chat_client.js
+++ chat_client.js
@@ -20,7 +20,7 @@ socket.on('greeting', function(msg){
 	var nick = msg.split(" ")[2];
 	var message = msg.split(": ")[1];
 	console.log(nick);
-	$('#nickName').html(nick);
+	$('.nickName').html(nick);
 	$('.program-title').html("MSN Messenger - " + nick  + " - (online)");
 	$('#chatWindow').html( $('#chatWindow').html() + '<p class="chatTitle">' + nick + ' säger:</p>' + '<p class="chatMessage">' + message + '</p>');
 });
@@ -28,7 +28,7 @@ socket.on('greeting', function(msg){
 socket.on('alert', function(msg){
 	if(msg.split(":")[0] == "nick")
 	{
-		$('#nickName').html(msg.split(":")[1]);
+		$('.nickName').html(msg.split(":")[1]);
 		$('.program-title').html("MSN Messenger - " + msg.split(":")[1]  + " - (online)");
 	}
 });
