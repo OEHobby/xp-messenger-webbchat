57246bce45bd68773fffe40fed51991437b99565
oskaremilsson <oskaremilsson@gmail.com>
2014-09-14 01:28:36 +0200

    New feature! Restore functionality!

--- js/mini_maxi.js
+++ js/mini_maxi.js
@@ -21,11 +21,27 @@ pickup.addEventListener("click", function(){
 });
 
 var offset = 7;
-var maxi = document.getElementById("maximize-button");
-maxi.addEventListener("click", function(){maximize(offset);});
+var isMaximized = false;
 
-function maximize(offset)
+var maxi = document.getElementById("maximize-button");
+maxi.addEventListener("click", function(){
+	if(!isMaximized)
+	{
+		saveWindow();
+		maximizeWindow(offset);
+		isMaximized = true;
+	}
+	else
+	{
+		restoreWindow();
+		isMaximized = false;
+	}
+	    
+});
+
+function maximizeWindow(offset)
 {
+	saveWindow();
 	//maximize window
 	$("#window").width($(window).innerWidth() - offset);
 	$("#window").height($(window).innerHeight() - offset);
@@ -42,5 +58,58 @@ function maximize(offset)
 	$("#chatWindow").width( $(window).width() - $("#rightBar").width()  - (offset + 10));
 	$("#chatWindow").height( $(window).height() - ($("#head").height() + $("#bottomBar").height() + $("#xp-bar").height() + 17) );
 	$("#right-bar").height($(window).height() - offset);
+}
 
+//define all the variabbles needed for the save/restore feature
+var winWidth;
+var winHeight;
+var headWidth;
+var bottomBarWidth;
+var xpBarWidth;
+var nickWrapperWidth;
+var textfieldWidth;
+var mWidth;
+var smileyBarWidth;
+var chatWindowWidth;
+var chatWindowHeight;
+var rightBarHeight;
+
+var windowPosX;
+var windowPosY;
+
+function saveWindow()
+{
+	winWidth = $("#window").width();
+	winHeight = $("#window").height();
+	headWidth = $("#head").width();
+	bottomBarWidth = $("#bottomBar").width();
+	xpBarWidth = $("#xp-bar").width();
+	nickWrapperWidth = $("#nickwrapper").width();
+	textfieldWidth = $("#text-field").width();
+	mWidth = $("#m").width();
+	smileyBarWidth = $("#smiley-bar").width();
+	chatWindowWidth = $("#chatWindow").width();
+	chatWindowHeight = $("#chatWindow").height();
+	rightBarHeight = $("#right-bar").height();
+
+	windowPosX = $("#window").offset().left;
+	windowPosY = $("#window").offset().top;
+}
+
+function restoreWindow()
+{
+	$("#window").width(winWidth);
+	$("#window").height(winHeight);
+	$("#head").width(headWidth);
+	$("#bottomBar").width(bottomBarWidth);
+	$("#xp-bar").width(xpBarWidth);
+	$("#nickwrapper").width(nickWrapperWidth);
+	$("#text-field").width(textfieldWidth);
+	$("#m").width(mWidth);
+	$("#smiley-bar").width(smileyBarWidth);
+	$("#chatWindow").width(chatWindowWidth);
+	$("#chatWindow").height(chatWindowHeight);
+	$("#right-bar").height(rightBarHeight);
+
+	$("#window").offset({ top: windowPosY, left: windowPosX });
 }
