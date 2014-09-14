ac36e948756cebcc7bc14ac801fc29bf45c5c5f0
oskaremilsson <oskaremilsson@gmail.com>
2014-09-14 02:07:49 +0200

    Makes smileys in the nick possible

--- js/chat_client.js
+++ js/chat_client.js
@@ -9,13 +9,14 @@ $('form').submit(function(){
 
 socket.on('chat message', function(msg){
 	console.log(msg);
-	var nick = msg.split(":")[0];
+	var nick = msg.split(": ")[0];
 	console.log("nick: " + nick);
-	msg = msg.split(": ")[1];
-	msg = msg.replace(/[\u00A0-\u9999<>\&]/gim, function(i) {
-        return '&#'+i.charCodeAt(0)+';';
-    });
+	msg = msg.slice(msg.indexOf(": ")+2, msg.length); //message is from : and to the end.
+	msg = secureString(msg);
+    nick = secureString(nick);
+
     msg = createSmileys(msg);
+    nick = createSmileys(nick);
 	$('#chatWindow').html( $('#chatWindow').html() + '<p class="chatTitle">' + nick + ' said:</p>' + '<p class="chatMessage">' + msg + '</p>');
 	autoScroll();
 	console.log($("#chatWindow")[0].scrollHeight);
@@ -26,30 +27,32 @@ socket.on('chat message', function(msg){
   });
 
 socket.on('greeting', function(msg){
+	msg = createSmileys(msg);
 	$('#chatWindow').html( $('#chatWindow').html() + '<p class="chatTitle">Boten Anna said:</p>' + '<p class="chatMessage">' + msg + '.</p>');
 	autoScroll()
 });
 
 socket.on('privmsg', function(msg){
 	console.log("got a priv message from " + nick);
-	var nick = msg.split(":")[0];
-	msg = msg.split(": ")[1];
-	msg = msg.replace(/[\u00A0-\u9999<>\&]/gim, function(i) {
-        return '&#'+i.charCodeAt(0)+';';
-    });
+	var nick = msg.split(": ")[0];
+	msg = msg.slice(msg.indexOf(": ")+2, msg.length);
+	msg = secureString(msg);
+	nick = secureString(nick);
+    msg = createSmileys(msg);
+    nick = createSmileys(nick);
 	$('#chatWindow').html( $('#chatWindow').html() + '<p class="chatTitle">' + nick + ' said in private:</p>' + '<p class="chatMessage">' + msg + '.</p>');
 	autoScroll()
 });
 
 socket.on('alert', function(msg){
 	var alert = msg.split(":")[0];
-	var info = msg.split(":")[1];
+	var info = msg.slice(msg.indexOf(":")+1, msg.length);
 	switch(alert)
 	{
 		case 'nick':
 			myNickname = info;
-			$('.nickName').html(info);
-			$('.program-title').html("MSN Messenger - " + info + " - (online)")
+			$('.nickName').html(myNickname);
+			$('.program-title').html("MSN Messenger - " + myNickname + " - (online)")
 			break;
 		case 'chatters':
 			$('.nickStatus').html("It's " + info + " online right now.");
@@ -70,4 +73,11 @@ socket.on('alert', function(msg){
 function autoScroll()
 {
 	$("#chatWindow").scrollTop($("#chatWindow")[0].scrollHeight);
+}
+
+function secureString(str)
+{
+	return str.replace(/[\u00A0-\u9999<>\&]/gim, function(i) {
+        		return '&#'+i.charCodeAt(0)+';';
+   			});
 }
